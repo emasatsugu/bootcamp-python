af3c172f8bef58c5168da2f18cf4e5cb2931c892
Erin Masatsugu <erin.masatsugu@gmail.com>
2013-11-18 01:41:44 -0500

    finished problem 10

--- exercises-more/exercises.py
+++ exercises-more/exercises.py
@@ -83,7 +83,12 @@ def word_frequency(lst):
 # in the list, and the number of times the word appears. If the list is empty, return None.
 # e.g. most_frequent_word(["a", "a", "aaa", "b", "b", "b"]) == ("b", 3)
 def most_frequent_word(lst):
-    return None
+    if len(lst) == 0:
+        return None
+    freqs = word_frequency(lst)
+    sortedfreqs = sorted(freqs.items(), key=lambda (word,count): count, reverse=True)
+    return sortedfreqs[0]
+    
 
 # PROB 11
 # Compares the two lists and finds all the positions that are mismatched in the list.
